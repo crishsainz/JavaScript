726505f3a92d698dc2ccde62664e270f207e007b
crishsainz <122748367+crishsainz@users.noreply.github.com>
2023-01-29 20:26:07 +0100

    ejercicio 1

---
+++ Ejercicio 1/index.js
@@ -0,0 +1,10 @@
+let alto;
+let ancho;
+let largo;
+alto = 13;
+ancho = 16;
+largo = 23;
+const volumenCubo = alto * ancho * largo;
+const areaCubo = ancho *largo;
+console.log("Volumen del Cubo", volumenCubo + " cm3");
+console.log("Area del cubo", areaCubo + " cm2")
