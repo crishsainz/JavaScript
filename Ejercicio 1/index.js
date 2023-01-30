651caee8c75e04bbf121a38f8002e678624efbdc
crishsainz <122748367+crishsainz@users.noreply.github.com>
2023-01-30 22:43:35 +0100

    Update index.js

--- Ejercicio 1/index.js
+++ Ejercicio 1/index.js
@@ -1,10 +1,23 @@
-let alto;
-let ancho;
-let largo;
-alto = 13;
-ancho = 16;
-largo = 23;
-const volumenCubo = alto * ancho * largo;
-const areaCubo = ancho *largo;
-console.log("Volumen del Cubo", volumenCubo + " cm3");
-console.log("Area del cubo", areaCubo + " cm2")
+// base 16 * 23 cm
+// altura 13 cm
+
+const volumen = 13 * 16 * 23;
+console.log("Volumen del cubo", volumen + " cm3");
+
+// area = area lateral + 2*(altura*base)
+const areaLateral = 13*23;
+const areaBase = 16*23;
+const areaTotal = areaLateral + 2*areaBase;
+console.log("areaTotal", areaTotal + " cm2");
+
+
+// Generalización
+const base = 13;
+const altura = 23;
+const ancho = 16;
+
+const volumen2 = base * altura * ancho;
+console.log("volumen2", volumen2);
+
+const area2 = base*altura + 2*altura*ancho;
+console.log("area2", area2);
