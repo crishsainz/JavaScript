f958764a7ac88a49d10ebc1b989572b22cb70753
crishsainz <122748367+crishsainz@users.noreply.github.com>
2023-01-31 20:00:53 +0100

    Ejercicio 2

---
+++ Ejercicio 2/index.js
@@ -0,0 +1,11 @@
+//Según el temario
+const diaCumpleaños = {
+    dia: 21,
+    mes: 'abril'
+}
+console.log("Mi cumpleaños es el " + diaCumpleaños.dia + " de " + diaCumpleaños.mes);
+
+// A mi manera
+const dia = 21;
+const mes = "abril";
+console.log ("Mi cumpleaños es el " + dia + " de " + mes);
