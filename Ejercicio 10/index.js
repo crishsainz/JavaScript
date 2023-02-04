ceb50c7f56fc70c4d9d8cefcc8a77cdff611a0f9
crishsainz <122748367+crishsainz@users.noreply.github.com>
2023-02-04 19:42:51 +0100

    Ejercicio 10

---
+++ Ejercicio 10/index.js
@@ -0,0 +1,9 @@
+function checkNumber (numero) {
+    if (numero%2===0) {
+        console.log ("El número " + numero + " es par");
+    }
+        else {
+            console.log ("El número " + numero + " es impar");
+        }
+}
+alert ("Introducir 'checkNumber(número)' en el inspector")
